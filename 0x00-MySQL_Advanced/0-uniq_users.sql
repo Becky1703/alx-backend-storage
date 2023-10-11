909d57d8c93b1deb5a5b1eb22810334cd2bf65d1
Becky1703 <oluwabukunmia@gmail.com>
2023-10-11 20:32:40 +0000

    unique user

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- sql script that creates a table user and adds different attributes
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
