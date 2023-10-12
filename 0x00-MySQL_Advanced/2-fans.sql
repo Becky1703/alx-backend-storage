cf1b2a18aae43ddb857661f7084a094e481228d3
Becky1703 <oluwabukunmia@gmail.com>
2023-10-12 09:46:10 +0000

    unique fans

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,5 @@
+-- script ranks country origins of bands and orders them by number of non unique fans.
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
