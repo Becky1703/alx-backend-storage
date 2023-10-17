d439ac4846a39e9e6fdf033baa3a09b7a8543fb1
Becky1703 <oluwabukunmia@gmail.com>
2023-10-17 12:06:32 +0000

    pycodestyle

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-""" Function to list all documents"""
-import pymongo
+""" Function to list all document"""
 
 
 def list_all(mongo_collection):
     """fuction lists all collections and returns empty list
      if no document is found"""
     return [doc for doc in mongo_collection.find()]
-
