d07b3e2920cb4093951eb62ab9ebe7b0eaed9b13
Becky1703 <oluwabukunmia@gmail.com>
2023-10-18 13:23:26 +0000

    get

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """REdis Server"""
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -17,3 +17,19 @@ class Cache():
         data_key = str(uuid.uuid4())
         self._redis.set(data_key, data)
         return data_key
+
+    def get(self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        """Function retrieves a value from a Redis data storage"""
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """Function retrieves a string value from a Redis data storage"""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Functioon retrieves an integer value from a Redis data storage"""
+        return self.get(key, lambda x: int(x))
