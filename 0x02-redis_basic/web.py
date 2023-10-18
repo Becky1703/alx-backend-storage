f6daeb5031ebd2821c5d7978df6621ab02f47154
Becky1703 <oluwabukunmia@gmail.com>
2023-10-18 20:44:11 +0000

    corrected pycodestyle errors

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,7 +5,7 @@ import requests
 from functools import wraps
 from typing import Calllable
 
-#redis instance
+# redis instance
 redis_store = redis.Redis()
 
 
@@ -17,12 +17,13 @@ def data_cacher(method: Callable) -> Callable:
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
-            return result,decode('utf-8')
+            return result.decode('utf-8')
         result = method(url)
         redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
+
 
 @data_cacher
 def get_page(url: str) -> str:
